5b8b2c1fe595a89d57bc0ec3e384aea30f2fcb17
Darius Taylor II <dariustaylorii@dariuss-mbp.wifi.local.cmu.edu>
2022-10-06 17:33:23 -0400

    added sample review for testing & modified ranges for score integers

--- docs-core/src/main/resources/db/update/dbupdate-028-0.sql
+++ docs-core/src/main/resources/db/update/dbupdate-028-0.sql
@@ -1,9 +1,8 @@
-create cached table DOCUMENT_REVIEWS ( REVIEWER_NAME varchar(40) not null, REVIEWED_DOCUMENT_ID int not null, ACADEMIC_SCORE int not null constraint ONE_THROUGH_TEN check (ACADEMIC_SCORE between 0 and 11), EXTRACURRICULR_SCORE int not null constraint ONE_THROUGH_TEN check (EXTRACURRICULR_SCORE between 0 and 11), ATHLETICS_SCORE int not null constraint ONE_THROUGH_TEN check (ATHLETICS_SCORE between 0 and 11), PERSONAL_FIT_SCORE int not null constraint ONE_THROUGH_TEN check (PERSONAL_FIT_SCORE between 0 and 11), DATE_CREATED datetime, primary key (REVIEWER_NAME) );
+create cached table DOCUMENT_REVIEWS ( REVIEWER_NAME varchar(40) not null, REVIEWED_DOCUMENT_ID int not null, ACADEMIC_SCORE int not null constraint ONE_THROUGH_TEN check (ACADEMIC_SCORE between 1 and 10), EXTRACURRICULR_SCORE int not null constraint ONE_THROUGH_TEN check (EXTRACURRICULR_SCORE between 1 and 10), ATHLETICS_SCORE int not null constraint ONE_THROUGH_TEN check (ATHLETICS_SCORE between 1 and 10), PERSONAL_FIT_SCORE int not null constraint ONE_THROUGH_TEN check (PERSONAL_FIT_SCORE between 1 and 10), DATE_CREATED date, primary key (REVIEWER_NAME) );
 alter table DOCUMENT_REVIEWS add constraint FK_REVIEWED_DOCUMENT_ID foreign key (REVIEWED_DOCUMENT_ID) references T_DOCUMENT (DOC_ID_C) on delete restrict on update restrict;
 update T_CONFIG set CFG_VALUE_C = '28' where CFG_ID_C = 'DB_VERSION';
 
-
-
+insert into DOCUMENT_REVIEWS(REVIEWER_NAME, REVIEWED_DOCUMENT_ID, ACADEMIC_SCORE, EXTRACURRICULR_SCORE, ATHLETIC_SCORE, PERSONAL_FIT_SCORE, DATE_CREATED ) values('Sample Student', '1', '5','5','5','5', '2022-10-04');
 
 
 
