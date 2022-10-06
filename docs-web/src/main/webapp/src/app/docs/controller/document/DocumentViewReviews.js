17e80ae1fc820336e43d5ad7ca55c1a1400b117c
Philip Kaufholz <pkaufhol@andrew.cmu.edu>
2022-10-06 19:23:43 -0400

    add skeleton of add review form

--- docs-web/src/main/webapp/src/app/docs/controller/document/DocumentViewReviews.js
+++ docs-web/src/main/webapp/src/app/docs/controller/document/DocumentViewReviews.js
@@ -3,6 +3,11 @@
 /**
  * Document view reviews controller.
  */
-angular.module('docs').controller('DocumentViewReviews', function($scope) {
-
+angular.module('docs').controller('DocumentViewReviews', function($scope, $stateParams, Restangular) {
+  Restangular.one('reviews').get({
+    documentId: $stateParams.id
+  }).then(function(data) {
+    console.log(data)
+    $scope.reviews = data.reviews;
+  });
 });
